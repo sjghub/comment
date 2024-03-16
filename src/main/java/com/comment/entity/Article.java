8549a3416bd2076523bcc537d69426d00be00778
esfuerzo <dkdldrha@naver.com>
2024-03-16 19:31:18 +0900

    page to page(redirect,return)

--- src/main/java/com/comment/entity/Article.java
+++ src/main/java/com/comment/entity/Article.java
@@ -4,14 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 @Entity
 public class Article {
     @Id
