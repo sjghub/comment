1d0c6bd7334053ad0cc8512656a95ad6519b34d4
esfuerzo <dkdldrha@naver.com>
2024-03-20 15:46:49 +0900

    show comments in article

--- src/main/java/com/comment/firstproject/controller/ArticleController.java
+++ src/main/java/com/comment/firstproject/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.comment.firstproject.controller;
 
 import com.comment.firstproject.dto.ArticleForm;
+import com.comment.firstproject.dto.CommentDto;
 import com.comment.firstproject.entity.Article;
 import com.comment.firstproject.repository.ArticleRepository;
+import com.comment.firstproject.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,6 +21,8 @@ import java.util.List;
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+    @Autowired
+    private CommentService commentService;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -45,9 +49,10 @@ public class ArticleController {
         log.info("id = " + id);
         // 1. id를 조회해 데이터 가져오기
         Article articleEntity = articleRepository.findById(id).orElse(null);
-
+        List<CommentDto> commentsDtos = commentService.comments(id);
         // 2. 모델에 데이터 등록하기
         model.addAttribute("article", articleEntity);
+        model.addAttribute("commentDtos", commentsDtos);
         // 3. 뷰 페이지 반환
         return "articles/show";
     }
